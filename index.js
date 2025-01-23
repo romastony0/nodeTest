ec6263bf01cc8f9b5e19fc8332b30aa142a0ce3d
romastony0 <romastony0@gmail.com>
2025-01-23 18:46:36 +0530

    New Route Created using PUT and POST

--- index.js
+++ index.js
@@ -10,7 +10,25 @@ const port = 3000;
 app.use(express.json());
 app.use("/", sampleRoute);
 
-app.post("/login", async (req, res) => {
+//user Login
+app.post("/userLogin", async (req, res) => {
+  try {
+    const user = await studentModel.findOne({ email: req.body.email });
+    const password = await studentModel.findOne({
+      password: req.body.password,
+    });
+    if (user && password) {
+      res.status(200).json({ message: "User Found Successfully" });
+    } else {
+      res.status(404).json({ message: "User Not Found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//user Sign Up
+app.post("/userSignUp", async (req, res) => {
   try {
     await studentModel.create(req.body);
     res.status(201).json({ message: "User Created Successfully" });
@@ -18,10 +36,31 @@ app.post("/login", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+//user Delete
+app.delete("/userDelete/:user_id", async (req, res) => {
+  try {
+    await studentModel.deleteOne({ user_id: req.params.user_id });
+    res.status(200).json({ message: "User Deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//user Update
+app.put("/userUpdate/:user_id", async (req, res) => {
+  try {
+    await studentModel.updateOne({ user_id: req.params.user_id }, req.body);
+    res.status(200).json({ message: "User Updated Successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 mongoose
-  .connect(
-    "mongodb+srv://romastony0:GWlCJ7jhvePGnjtK@backenddbtraining.e225l.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDBTraining"
-  )
+  // .connect(
+  //   "mongodb+srv://romastony0:GWlCJ7jhvePGnjtK@backenddbtraining.e225l.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDBTraining"
+  // )
   .then(() => {
     console.log("Connected to DB");
 
--- models/student.model.js
+++ models/student.model.js
@@ -1,26 +1,37 @@
 import mongoose from "mongoose";
+import AutoIncrement from "mongoose-sequence";
 
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please Enter Your Name"],
+const studentSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: Number,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Please Enter Your Name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please Enter Your Email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please Enter Your Password"],
+    },
+    mobile: {
+      type: String,
+      required: [true, "Please Enter Your Mobile Number"],
+      unique: true,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please Enter Your Email"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please Enter Your Password"],
-  },
-  mobile: {
-    type: String,
-    required: [true, "Please Enter Your Mobile Number"],
-    unique: true,
-  },
-});
+  { timestamps: true } // Automatically manages createdAt and updatedAt
+);
 
-const Student = mongoose.model("studentLoginData", studentSchema);
+// Auto-increment user_id
+studentSchema.plugin(AutoIncrement(mongoose), { inc_field: "user_id" });
+
+const Student = mongoose.model("Student", studentSchema);
 
 export default Student;
